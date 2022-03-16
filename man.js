ecfedb8214bf7c2601465e4dfa2fe357e212462e
ThienBui1801 <buiquythien1801@gmail.com>
2022-03-16 22:10:21 +0700

    Updates

--- man.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const menu = document.querySelector('#menu');
-const btnMenu = document.querySelector('#btn-menu');
-
-btnMenu.addEventListener('click', () => {
-  menu.classList.toggle('invisible')
-});
