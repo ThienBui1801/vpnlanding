cefbd99df01ba18f669221745a28b5bde8b20532
ThienBui1801 <buiquythien1801@gmail.com>
2022-03-16 15:02:28 +0700

    first upload

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: [
+    "**/*.html"
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Roboto']
+      }
+    },
+    
+    screens: {
+      'pc': {'min': '1510px'},
+
+      'laptop': {'min': '1024px'},
+      // Laptop, PC, Tablet > 1024px
+
+      'tablet': {'min': '740px', 'max': '1023px'},
+      // Tablet
+
+      'mobile': {'max': '739px'},
+      // Mobile
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
